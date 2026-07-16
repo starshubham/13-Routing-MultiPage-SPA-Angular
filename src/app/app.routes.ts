6e0a8afff05d2cde21f3774f268d680d81343f0a
SHUBHAM SETH <shubham.seth@greenpaynetwork.co.in>
2026-07-16 14:58:07 +0530

    Working with Nested Routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from "@angular/router";
 import { TasksComponent } from "./tasks/tasks.component";
 import { NoTaskComponent } from "./tasks/no-task/no-task.component";
 import { UserTasksComponent } from "./users/user-tasks/user-tasks.component";
+import { NewTaskComponent } from "./tasks/new-task/new-task.component";
 
 export const routes: Routes = [
     {
@@ -11,5 +12,15 @@ export const routes: Routes = [
     { 
         path: 'users/:userId', // <your-domain>/users/<uid>
         component: UserTasksComponent,
+        children: [
+            {
+                path: 'tasks', // <your-domain>/users/<uid>/tasks
+                component: TasksComponent,
+            },
+            {
+                path: 'tasks/new',
+                component: NewTaskComponent, // <your-domain>/users/<uid>/tasks/new
+            }
+        ]
     },
 ]
