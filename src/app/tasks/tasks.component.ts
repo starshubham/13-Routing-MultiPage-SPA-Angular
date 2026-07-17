1937d49d19ed8cb3b55b02223514a440a7b87255
SHUBHAM SETH <shubham.seth@greenpaynetwork.co.in>
2026-07-17 23:55:13 +0530

    Using Query Parameters for Data Manipulation

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, DestroyRef, inject, input } from '@angular/core';
+import { Component, computed, DestroyRef, inject, input, signal } from '@angular/core';
 
 import { TaskComponent } from './task/task.component';
 import { Task } from './task/task.model';
@@ -15,17 +15,29 @@ import { ActivatedRoute, RouterLink } from '@angular/router';
 export class TasksComponent {
     userId = input.required<string>();
     // order = input<'asc' | 'desc'>();
-    order?: 'asc' | 'desc';
+    // order?: 'asc' | 'desc';
+    order = signal<'asc' | 'desc'>('desc');
     private tasksService = inject(TasksService);
     userTasks = computed(() => 
-      this.tasksService.allTasks().filter((t) => t.userId === this.userId())
+      // this.tasksService.allTasks().filter((t) => t.userId === this.userId())
+      this.tasksService
+        .allTasks()
+        .filter((t) => t.userId === this.userId())
+        .sort((a, b) => {
+          if (this.order() === 'desc') {
+            return a.id > b.id ? -1 : 1;
+          } else {
+            return a.id > b.id ? 1 : -1;
+          }
+        })
   );
   private activatedRoute = inject(ActivatedRoute);
   private destroyRef = inject(DestroyRef);
 
   ngOnInit(): void {
     const subscription = this.activatedRoute.queryParams.subscribe({
-      next: params => (this.order = params['order']),
+      // next: params => (this.order = params['order']),
+       next: params => (this.order.set(params['order'])),
     });
     
     this.destroyRef.onDestroy(() => {
