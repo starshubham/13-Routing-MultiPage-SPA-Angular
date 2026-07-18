8a35aca39f8f2b073497c1d78464aeadbf074ba8
SHUBHAM SETH <shubham.seth@greenpaynetwork.co.in>
2026-07-18 18:35:06 +0530

    Using Lazy Loading & Routing to Lazy-load Services

--- src/app/users/users.routes.ts
+++ src/app/users/users.routes.ts
@@ -1,26 +1,34 @@
 import { Routes } from "@angular/router";
 import { canLeaveEditPage, NewTaskComponent } from "../tasks/new-task/new-task.component";
 import { resolveUserTasks, TasksComponent } from "../tasks/tasks.component";
+import { TasksService } from "../tasks/tasks.service";
 
 export const routes: Routes = [
     {
-        path: '', // <your-domain>/users/<uid>/
-        redirectTo: 'tasks',
-        pathMatch: 'full'
-    },
-    {
-        path: 'tasks', // <your-domain>/users/<uid>/tasks
-        component: TasksComponent,
-        // loadComponent: () => 
-        //     import('../tasks/tasks.component').then(mod => mod.TasksComponent),
-        runGuardsAndResolvers: 'always',
-        resolve: {
-            userTasks: resolveUserTasks,
-        },
-    },
-    {
-        path: 'tasks/new',
-        component: NewTaskComponent, // <your-domain>/users/<uid>/tasks/new
-        canDeactivate: [canLeaveEditPage]
+        path: '',
+        providers: [TasksService],
+        children: [
+            {
+                path: '', // <your-domain>/users/<uid>/
+                redirectTo: 'tasks',
+                pathMatch: 'full'
+            },
+            {
+                path: 'tasks', // <your-domain>/users/<uid>/tasks
+                component: TasksComponent,
+                // loadComponent: () => 
+                //     import('../tasks/tasks.component').then(mod => mod.TasksComponent),
+                runGuardsAndResolvers: 'always',
+                resolve: {
+                    userTasks: resolveUserTasks,
+                },
+            },
+            {
+                path: 'tasks/new',
+                component: NewTaskComponent, // <your-domain>/users/<uid>/tasks/new
+                canDeactivate: [canLeaveEditPage]
+            }
+        ]
     }
+    
 ]
