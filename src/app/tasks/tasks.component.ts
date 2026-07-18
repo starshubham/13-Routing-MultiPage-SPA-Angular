baa69f3b67630980b6040d4bf3502755a4430e82
SHUBHAM SETH <shubham.seth@greenpaynetwork.co.in>
2026-07-18 18:17:39 +0530

    Implementing Route-based Lazy Loading

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -45,23 +45,3 @@ export class TasksComponent {
   //   });
   // }
 }
-
-export const resolveUserTasks: ResolveFn<Task[]> = (
-  activatedRouteSnapshot,
-  routerState
-) => {
-  const order = activatedRouteSnapshot.queryParams['order'];
-  const tasksService = inject(TasksService);
-  const tasks = tasksService
-    .allTasks()
-    .filter(
-      (task) => task.userId === activatedRouteSnapshot.paramMap.get('userId')
-    );
-
-  if (order && order === 'asc') {
-    tasks.sort((a, b) => (a.id > b.id ? 1 : -1));
-  } else {
-    tasks.sort((a, b) => (a.id > b.id ? -1 : 1));
-  }
-  return tasks.length ? tasks : [];
-}
