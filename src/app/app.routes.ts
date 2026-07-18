888298d6497d1440ceec5a22c3862d2bfd2bc908
SHUBHAM SETH <shubham.seth@greenpaynetwork.co.in>
2026-07-18 16:20:01 +0530

    Setting & Resolving Titles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from "@angular/router";
 import { NoTaskComponent } from "./tasks/no-task/no-task.component";
-import { resolveUserName, UserTasksComponent } from "./users/user-tasks/user-tasks.component";
+import { resolveTitle, resolveUserName, UserTasksComponent } from "./users/user-tasks/user-tasks.component";
 import { NotFoundComponent } from "./not-found/not-found.component";
 import { routes as userRoutes } from "./users/users.routes";
 
@@ -10,6 +10,7 @@ export const routes: Routes = [
         component: NoTaskComponent,
         // redirectTo: '/users/u1',
         // pathMatch: 'full'
+        title: 'No task selected'
     },
     { 
         path: 'users/:userId', // <your-domain>/users/<uid>
@@ -20,7 +21,8 @@ export const routes: Routes = [
         },
         resolve: { 
             userName: resolveUserName
-        }
+        },
+        title: resolveTitle
     },
     {
         path: '**', // <your-domain>/*
