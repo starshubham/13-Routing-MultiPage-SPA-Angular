5a3d2f0fe0941bd1130ed5d56ea6ae889d356a3a
SHUBHAM SETH <shubham.seth@greenpaynetwork.co.in>
2026-07-18 16:11:11 +0530

    Controlling Route Resolver Execution

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -3,7 +3,7 @@ import { Component, computed, DestroyRef, inject, input, signal } from '@angular
 import { TaskComponent } from './task/task.component';
 import { Task } from './task/task.model';
 import { TasksService } from './tasks.service';
-import { ActivatedRoute, RouterLink } from '@angular/router';
+import { ActivatedRoute, ResolveFn, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-tasks',
@@ -13,35 +13,55 @@ import { ActivatedRoute, RouterLink } from '@angular/router';
   imports: [TaskComponent, RouterLink],
 })
 export class TasksComponent {
-    userId = input.required<string>();
-    // order = input<'asc' | 'desc'>();
-    // order?: 'asc' | 'desc';
-    order = signal<'asc' | 'desc'>('desc');
-    private tasksService = inject(TasksService);
-    userTasks = computed(() => 
-      // this.tasksService.allTasks().filter((t) => t.userId === this.userId())
-      this.tasksService
-        .allTasks()
-        .filter((t) => t.userId === this.userId())
-        .sort((a, b) => {
-          if (this.order() === 'desc') {
-            return a.id > b.id ? -1 : 1;
-          } else {
-            return a.id > b.id ? 1 : -1;
-          }
-        })
-  );
-  private activatedRoute = inject(ActivatedRoute);
-  private destroyRef = inject(DestroyRef);
+  userTasks = input.required<Task[]>();
+  userId = input.required<string>();
+  order = input<'asc' | 'desc'>();
+  // order?: 'asc' | 'desc';
+  // order = signal<'asc' | 'desc'>('desc');
 
-  ngOnInit(): void {
-    const subscription = this.activatedRoute.queryParams.subscribe({
-      // next: params => (this.order = params['order']),
-       next: params => (this.order.set(params['order'])),
-    });
-    
-    this.destroyRef.onDestroy(() => {
-      subscription.unsubscribe();
-    });
+  // private tasksService = inject(TasksService);
+  // userTasks = computed(() => 
+  //   // this.tasksService.allTasks().filter((t) => t.userId === this.userId())
+  //   this.tasksService
+  //     .allTasks()
+  //     .filter((t) => t.userId === this.userId())
+  //     .sort((a, b) => {
+  //       if (this.order() === 'desc') {
+  //         return a.id > b.id ? -1 : 1;
+  //       } else {
+  //         return a.id > b.id ? 1 : -1;
+  //       }
+  //     })
+  // );
+  // private activatedRoute = inject(ActivatedRoute);
+  // private destroyRef = inject(DestroyRef);
+  // ngOnInit(): void {
+  //   const subscription = this.activatedRoute.queryParams.subscribe({
+  //     // next: params => (this.order = params['order']),
+  //      next: params => (this.order.set(params['order'])),
+  //   });
+  //   this.destroyRef.onDestroy(() => {
+  //     subscription.unsubscribe();
+  //   });
+  // }
+}
+
+export const resolveUserTasks: ResolveFn<Task[]> = (
+  activatedRouteSnapshot,
+  routerState
+) => {
+  const order = activatedRouteSnapshot.queryParams['order'];
+  const tasksService = inject(TasksService);
+  const tasks = tasksService
+    .allTasks()
+    .filter(
+      (task) => task.userId === activatedRouteSnapshot.paramMap.get('userId')
+    );
+
+  if (order && order === 'asc') {
+    tasks.sort((a, b) => (a.id > b.id ? 1 : -1));
+  } else {
+    tasks.sort((a, b) => (a.id > b.id ? -1 : 1));
   }
+  return tasks.length ? tasks : [];
 }
